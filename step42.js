3128c239dc76c4bb744cad52e60e312adbb56392
belu20 <isanseptian20@gmail.com>
2024-11-08 22:19:24 +0700

    pyramid

---
+++ step42.js
@@ -0,0 +1,14 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+//   rows.push(i);
+    rows.push(character);
+}
+
+let result = "";
+for (const row of rows) {
+    result = result + row + "\n";
+   }
+   console.log(result);
